f722e49f40cccb85253b16f72c323df875e4816f
Cesar <cpimentelortiz23@gmail.com>
2025-09-23 11:08:29 -0400

    Onion Skeleton Class

---
+++ src/Onion.java
@@ -0,0 +1,17 @@
+public class Onion {
+    int binary;
+
+    public void Peel(int binary) {
+        // Under construction
+    }
+
+    public Onion(int binary) {
+        this.binary = binary;
+    }
+
+    public int flip(int number) {
+        if (number == 1) return 0;
+        else return 0;
+    }
+
+}
