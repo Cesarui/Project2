ebb62b1082d6624a1d78e86b3f88428988cdcde0
Cesar <cpimentelortiz23@gmail.com>
2025-09-24 17:18:58 -0400

    Recursive logic

--- src/Onion.java
+++ src/Onion.java
@@ -1,17 +1,51 @@
-public class Onion {
-    int binary;
+import java.util.Arrays;
 
-    public void Peel(int binary) {
-        // Under construction
+public class Onion {
+    int[] digits;
+    int[] result;
+
+    private int[] Peel(int[] digits) {
+        if (digits.length == 1) {
+            return new int[] {flip(digits[0])};
+        }
+        else {
+            int[] smallArr = new int[digits.length - 2];
+            for (int i = 1; i < digits.length - 1; i++) {
+                smallArr[i - 1] = digits[i];
+            }
+            Onion inner = new Onion(smallArr);
+            int[] innerResult = inner.getResult();
+
+            int[] full = new int[innerResult.length + 2];
+            full[0] = flip(digits[0]);
+            for (int i = 0; i < innerResult.length; i++) {
+                full[i + 1] = innerResult[i];
+            }
+            full[full.length - 1] = flip(digits[digits.length - 1]);
+
+            return full;
+        }
     }
 
-    public Onion(int binary) {
-        this.binary = binary;
+    public Onion(int[] digits) {
+        if (digits.length % 2 == 0) throw new IllegalArgumentException("Array lenght must be odd");
+        this.digits = digits;
+        this.result = Peel(digits);
+
     }
 
     public int flip(int number) {
         if (number == 1) return 0;
-        else return 0;
+        else return 1;
+    }
+
+    public int[] getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Flipped: " + Arrays.toString(result);
     }
 
 }
--- src/Tester.java
+++ src/Tester.java
@@ -3,6 +3,10 @@ import org.junit.Test;
 public class Tester {
     @Test
     public void test() {
-        
+        int[] myNums = {1,0,1,0,1};
+
+        Onion onion1 = new Onion(myNums);
+
+        System.out.println(onion1);
     }
 }
