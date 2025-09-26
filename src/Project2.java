d0b3fb4e71615029b6aa85d2cf023c20a200dd65
Cesar <cpimentelortiz23@gmail.com>
2025-09-25 22:19:49 -0400

    Can add individuals digits in a number to an array.

--- src/Project2.java
+++ src/Project2.java
@@ -1,7 +1,19 @@
 // Cesar Pimentel & Baheeja Muntasser
 
+import java.util.Scanner;
+
 public class Project2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type the number you'd like to flip: ");
+        int userNum = scanner.nextInt();
+
+        int[] userNumArr = Onion.numIntoArray(userNum);
+
+        Onion onion1 = new Onion(userNumArr);
+
+        System.out.println(onion1);
 
     }
 }
